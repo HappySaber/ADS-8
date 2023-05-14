06ffab5be55c5b29e2c456163d1f7ced4ac790ef
Stepan <115095505+HappySaber@users.noreply.github.com>
2023-05-14 22:02:02 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -8,43 +8,44 @@ void Train::addCage(bool light) {
   if (first == nullptr) {
     first = new Cage;
     first->light = light;
-    first->next->prev = first;
-
+    first->next = first;
+    first->prev = first;
   } else {
     cage = new Cage;
     cage->next = first;
     cage->prev = first->prev;
     cage->light = light;
     first->prev = cage;
-    first->next = first;
-    first->prev = first;
+    cage->prev->next = cage;
   }
 }
+
 
 int Train::getLength() {
     Cage* f = first;
     f->light = true;
     int count = 1;
+    if (f != nullptr) {
     while (true) {
-        f = f->next;
-        if (f->light == false) {
-          count++;
+      f = f->next;
+      if (f->light == false) {
+        count++;
+        countOp++;
+      } else {
+        f->light = false;
+        for (int i = 0; i < count; i++) {
+          f = f->prev;
           countOp++;
-        } else {
-          f->light = false;
-          for (int i = count; i > 0; i--) {
-            f = f->prev;
-            countOp++;
-          }
-          countOp++;
-          if (f->light = false)
-            return count;
-          else
-            count == 1;
         }
+        countOp++;
+        if (f->light == false)
+          return count;
+        else
+          count = 1;
+      }
     }
+  }
 }
-
 int Train::getOpCount() {
     return countOp;
 }
